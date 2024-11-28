0a84b57e5e8d61234f00898c7f6a9e62645cba20
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-11-28 13:53:41 -0300

    refactor(navbar): use tailwindcss approach

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -30,7 +30,7 @@ export const navigationItems = [
 export function Navbar() {
   const pathname = usePathname()
   return (
-    <nav className="no-print max-w-7xl mx-auto px-4 md:px-8 py-5 grid grid-cols-12">
+    <nav className="max-w-7xl mx-auto px-4 md:px-8 py-5 grid grid-cols-12 print:hidden">
       <div className="col-span-6 flex md:col-span-3">
         <Link href="/">
           <h1 className="text-3xl font-semibold">
