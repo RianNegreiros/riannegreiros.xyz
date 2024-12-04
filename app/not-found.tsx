a5750fdd11014d9ae5c27b7f6560bf44da7d81f3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-04 09:59:31 -0300

    refactor: not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,13 +8,13 @@ export default function NotFound() {
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl transition-transform hover:scale-110">
             404
           </h1>
-          <p className="text-gray-500">
+          <p className="text-gray-700">
             Não foi possível encontrar o recurso solicitado
           </p>
         </div>
         <Link
           href="/"
-          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-200 dark:focus-visible:ring-gray-300"
           prefetch={false}
         >
           Voltar ao site
