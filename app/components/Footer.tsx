135294d379dbd5f7fbf05c3d75ae5e30135a0b12
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-12-05 15:16:26 -0300

    fix: reduce footer size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -19,13 +19,13 @@ const navigationItems = [
 
 export default function Footer() {
   return (
-    <footer className="p-4 md:p-8 lg:p-10">
-      <hr className="my-3 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <footer className="px-4 md:px-8 lg:px-10">
+      <hr className="mb-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="mx-auto max-w-screen-xl text-center">
         <Link href="/" className="flex justify-center items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <Image src="/favicon.ico" alt="Logo" className="mr-2 h-8" width={32} height={32} />
         </Link>
-        <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+        <ul className="flex flex-wrap justify-center items-center text-gray-900 dark:text-white">
           {navigationItems.map((item, index) => (
             <li key={index}>
               <Link className="mr-4 hover:underline md:mr-6" href={item.href}>
@@ -35,11 +35,10 @@ export default function Footer() {
           ))}
         </ul>
         <div className="px-4 py-6 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© {new Date().getFullYear()} <Link href="https://www.riannegreiros.dev/">riannegreiros.dev</Link>. Todos os direitos reservados.
-          </span>
+          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© {new Date().getFullYear()} <Link className='hover:underline' href="https://www.riannegreiros.dev/">riannegreiros.dev</Link>. Todos os direitos reservados.</span>
           <div className="flex sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
             {RESUME_DATA.contact.social.map((social) => (
-              <a href={social.url} className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
+              <a href={social.url} key={social.name} className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
                 <social.icon className="w-4 h-4" />
               </a>
             ))}
