c5980db5cea6a85ff96e50adc81db045f65b1076
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-01-09 10:46:40 -0300

    refactor(timeline): sanity queries

--- app/action.tsx
+++ app/action.tsx
@@ -1,7 +1,7 @@
 'use server'
 
 import TimelineItem from './components/TimelineItem'
-import { TimelineItemProps } from './lib/interface'
+import { PortfolioItem } from './lib/interface'
 import { client } from './lib/sanity'
 
 export const getTotalPosts = async () => {
@@ -10,7 +10,7 @@ export const getTotalPosts = async () => {
 }
 
 export const getTotal = async () => {
-  const query = `count(*[_type != "translation.metadata" && _type != "sanity.imageAsset"])`
+  const query = `count(*[_type in ['post', 'project']])`
   return client.fetch(query)
 }
 
@@ -20,7 +20,7 @@ export const getAll = async (
 ) => {
   const start = pageNum * itemsPerPage
   const end = start + itemsPerPage
-  const query = `*[_type != "translation.metadata" && _type != "sanity.imageAsset"] | order(firstPublishedDate desc) [${start}...${end}] {
+  const query = `*[_type == "post" || _type == "project"] | order(firstPublishedDate desc) [${start}...${end}] {
         title,
         _id,
         _type,
@@ -33,7 +33,7 @@ export const getAll = async (
 
   const data = await client.fetch(query)
 
-  return data.map((item: TimelineItemProps, index: number) => (
+  return data.map((item: PortfolioItem, index: number) => (
     <TimelineItem key={item._id} {...item} index={index} />
   ))
 }
