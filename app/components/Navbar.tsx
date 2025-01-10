23f3d1709293a9d535b3c4c86813e1ab3b21d183
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-01-10 17:45:40 -0300

    fix: center nav items with the page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,30 +1,25 @@
 'use client'
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { ModeToggle } from './ModeToggle'
 import { MobileMenu } from './MobileMenu'
 import { navigationItems } from '@/data/navigation-items'
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
 
 export function Navbar() {
   const pathname = usePathname()
+
   return (
-    <nav className="bg-background shadow-sm print:hidden">
+    <nav className="bg-background shadow-sm" aria-label="Main navigation">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <Link href="/" className="flex-shrink-0">
-            <h1 className="text-3xl font-semibold">
-              Rian <span className="text-blue-500">Negreiros</span>
+          <Link href="/" className="flex-shrink-0 md:w-1/4">
+            <h1 className="text-3xl font-semibold whitespace-nowrap">
+              Rian <span className="text-primary">Negreiros</span>
             </h1>
           </Link>
-          <div className="hidden md:flex flex-1 justify-center items-center">
+          <div className="hidden md:flex flex-1 justify-center">
             <NavigationMenu>
               <NavigationMenuList>
                 {navigationItems.map((item, index) => (
@@ -42,11 +37,9 @@ export function Navbar() {
               </NavigationMenuList>
             </NavigationMenu>
           </div>
-          <div className="flex items-center">
-            <div className="ml-4 flex items-center md:ml-6">
-              <ModeToggle />
-            </div>
-            <div className="flex justify-end sm:hidden ml-4">
+          <div className="flex items-center space-x-4 md:w-1/4 justify-end">
+            <ModeToggle />
+            <div className="md:hidden">
               <MobileMenu />
             </div>
           </div>
