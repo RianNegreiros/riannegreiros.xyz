bfbb6b7c61b21a01e7868a63248651fa572d2269
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-02-17 07:18:18 -0300

    fix: disallow `/api/`, `/health`, and other unwanted pages
    closes #321

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,22 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/projects',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/projects',
+          '/api/',
+          '/health',
+          '/hooks/',
+          '/components/',
+          '/swagger/',
+          '/prompt/',
+          '/github.com/',
+        ],
+      },
+    ],
     sitemap: 'https://www.riannegreiros.xyz/sitemap.xml',
   }
 }
