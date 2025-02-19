a60ce58e250187b6d7a9886d45c3888abefa2f41
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-02-19 09:56:08 -0300

    refactor: allow `/api/sitemap` and `/api/rss` on `robots.ts`
    closes #331

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,11 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: [
+          '/',
+          '/api/sitemap',
+          '/api/rss'
+        ],
         disallow: [
           '/projects',
           '/api/',
