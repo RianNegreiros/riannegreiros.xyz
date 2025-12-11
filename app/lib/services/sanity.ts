a14399549fb4759e17a585434d01a063c56f1e5d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-12-11 08:28:52 -0300

    fix: The default export of @sanity/image-url has been deprecated. Use the named export `createImageUrlBuilder` instead.

--- app/lib/services/sanity.ts
+++ app/lib/services/sanity.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { createImageUrlBuilder } from '@sanity/image-url'
 
 const client = createClient({
   apiVersion: '2024-03-13',
@@ -10,9 +9,9 @@ const client = createClient({
   perspective: 'published',
 })
 
-const builder = imageUrlBuilder(client)
+const builder = createImageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source)
 }
 
