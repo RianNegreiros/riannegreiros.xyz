8ea6bbd3e88c98c984af41e07879593b2f0ba9fb
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-12-18 11:14:03 -0300

    refactor: update rss to use a modern library and fix the script not getting the base url from node enviroment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      Sitemap({
+        hostname: env.VITE_BASE_URL || "http://localhost:5173",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    base: env.VITE_BASE_URL,
+  };
 });
